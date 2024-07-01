32deb107d8de507c2e55bf84f13700411df73353
totolabrico <thomas-barbe@hotmail.com>
2024-07-01 15:11:03 +0200

    edit Directory and HTMLPage

--- src/Response.cpp
+++ src/Response.cpp
@@ -9,7 +9,7 @@ Response::Response()
     content = dir.GetHtml();
     ss << content.size();
     text_ = "HTTP/1.1 200 OK\r\nContent-Length: " + ss.str() + "\r\n\r\n";
-    text_ += dir.GetHtml();
+    text_ += content;
 }
 
 Response::~Response()
