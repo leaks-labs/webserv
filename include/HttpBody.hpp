ba7e14714919cd02d5afe37f905ed4d70901d19d
totolabrico <thomas-barbe@hotmail.com>
2024-07-25 16:45:20 +0200

    edit uint64 to long

--- include/HttpBody.hpp
+++ include/HttpBody.hpp
@@ -48,7 +48,7 @@ class HttpBody {
         int         mode_;
         std::string body_;
         std::string buffer_;
-        int64_t     chunk_size_;
+        long     chunk_size_;
         bool        is_trailer_;
         HttpHeader* headers_;
 };
