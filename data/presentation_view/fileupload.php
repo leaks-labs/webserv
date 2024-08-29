ad72a3caea893c4769ef7818701756a5eda0cc1f
martinthonon <mrt.thonon@gmail.com>
2024-08-29 20:57:42 +0200

    Some logs to help debugging

--- data/presentation_view/fileupload.php
+++ data/presentation_view/fileupload.php
@@ -12,10 +12,37 @@ if (!isset($_FILES['file'])) {
     exit;
 }
 
+
 if ($_FILES['file']['error'] != UPLOAD_ERR_OK) {
-    echo 'erreur lors de l\'upload du fichier2';
-    exit;
+   switch ($_FILES['file']['error']) {
+       case UPLOAD_ERR_INI_SIZE:
+           echo 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
+           break;
+       case UPLOAD_ERR_FORM_SIZE:
+           echo 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
+           break;
+       case UPLOAD_ERR_PARTIAL:
+           echo 'The uploaded file was only partially uploaded.';
+           break;
+       case UPLOAD_ERR_NO_FILE:
+           echo 'No file was uploaded.';
+           break;
+       case UPLOAD_ERR_NO_TMP_DIR:
+           echo 'Missing a temporary folder.';
+           break;
+       case UPLOAD_ERR_CANT_WRITE:
+           echo 'Failed to write file to disk.';
+           break;
+       case UPLOAD_ERR_EXTENSION:
+           echo 'A PHP extension stopped the file upload.';
+           break;
+       default:
+           echo 'Unknown error occurred during file upload.';
+           break;
+   }
+   exit;
 }
+
 
 $fileTmpPath = $_FILES['file']['tmp_name'];
 $fileName = $_FILES['file']['name'];
