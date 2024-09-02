237f436f4bd321ce7d76bd6c76b4668fdf754237
shinfray <shinfray@student.s19.be>
2024-09-02 12:31:12 +0200

    cleaning file

--- data/default_view/fileget.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title = isset($_GET['title']) ? htmlspecialchars($_GET['title']) : '';
-echo "<!DOCTYPE html>\n";
-echo "<html>\n<body>\n";
-echo "<h1>$title</h1>\n";
-print_r($_GET);
-print_r($_POST);
-echo "</html>\n</body>\n";
-?>
